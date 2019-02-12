950ff4c1dd262f44d9c38fdd061956c5e5bca6bf
Stefano Macconi <stefano.macconi@gmail.com>
2019-02-12 09:50:18 +0100

    charts

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,12 +1,41 @@
 <template>
-  <div>
-    <b-progress :value="counter" :max="max" show-progress animated></b-progress>
-    <b-progress class="mt-1" :max="max" show-value>
-      <b-progress-bar :value="counter*(6/10)" variant="success"></b-progress-bar>
-      <b-progress-bar :value="counter*(2.5/10)" variant="warning"></b-progress-bar>
-      <b-progress-bar :value="counter*(1.5/10)" variant="danger"></b-progress-bar>
-    </b-progress>
-    <b-btn class="mt-4" @click="clicked">Click me</b-btn>
+  <div class="container">
+    <div class="row">
+      <div class="col-md">
+        <b-progress height="50px" class="mt-1" :max="max" show-value>
+          <b-progress-bar :value="counter*(6/10)" variant="success"></b-progress-bar>
+          <b-progress-bar :value="counter*(2.5/10)" variant="warning"></b-progress-bar>
+          <b-progress-bar :value="counter*(1.5/10)" variant="danger"></b-progress-bar>
+        </b-progress>
+        <b-btn class="mt-4" @click="clicked">Click me</b-btn>
+      </div>
+      <div class="col-md">
+        <b-progress height="50px" class="mt-1" :max="max" show-value>
+          <b-progress-bar :value="counter*(6/10)" variant="success"></b-progress-bar>
+          <b-progress-bar :value="counter*(2.5/10)" variant="warning"></b-progress-bar>
+          <b-progress-bar :value="counter*(1.5/10)" variant="danger"></b-progress-bar>
+        </b-progress>
+        <b-btn class="mt-4" @click="clicked">Click me</b-btn>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md">
+        <b-progress height="50px" class="mt-1" :max="max" show-value>
+          <b-progress-bar :value="counter*(6/10)" variant="success"></b-progress-bar>
+          <b-progress-bar :value="counter*(2.5/10)" variant="warning"></b-progress-bar>
+          <b-progress-bar :value="counter*(1.5/10)" variant="danger"></b-progress-bar>
+        </b-progress>
+        <b-btn class="mt-4" @click="clicked">Click me</b-btn>
+      </div>
+      <div class="col-md">
+        <b-progress height="50px" class="mt-1" :max="max" show-value>
+          <b-progress-bar :value="counter*(6/10)" variant="success"></b-progress-bar>
+          <b-progress-bar :value="counter*(2.5/10)" variant="warning"></b-progress-bar>
+          <b-progress-bar :value="counter*(1.5/10)" variant="danger"></b-progress-bar>
+        </b-progress>
+        <b-btn class="mt-4" @click="clicked">Click me</b-btn>
+      </div>
+    </div>
   </div>
 </template>
 
