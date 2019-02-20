0aedd31b020943e723a6a2e2324a449015a7959d
Stefano Macconi <stefano.macconi@gmail.com>
2019-02-20 11:29:18 +0100

    final configurations

--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,5 @@ import App from './App'
 
 export const routes = [
     { path: '/', component: App },
-    { name: "gruppoCdL", path: '/:gruppoCdL', component: App}
+    { name: "gruppoCdL", path: ':gruppoCdL', component: App}
 ]
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,24 @@
+const { WebpackWarPlugin } = require('webpack-war-plugin');
+
 module.exports = {
-    publicPath: './'
+    publicPath: '/monitor',
+    configureWebpack: () => {
+        if (process.env.NODE_ENV === 'production') {
+            // mutate for production...
+            return {
+                plugins: [
+                    new WebpackWarPlugin({
+                        archiveName: 'monitor',
+                    })
+                ],
+                devServer: {
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                    }
+                }   
+            }
+        } else {
+            // mutate for development...
+        }
+    }
 }
